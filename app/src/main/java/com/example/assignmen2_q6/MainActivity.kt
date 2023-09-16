d6fafa3cc4bd5cdeb76c199e3ebecf70cf2584bd
Pizzawookiee <47388918+Pizzawookiee@users.noreply.github.com>
2023-09-15 22:44:39 -0400

    fixes for number input to handle decimal edge cases

--- app/src/main/java/com/example/assignmen2_q6/MainActivity.kt
+++ app/src/main/java/com/example/assignmen2_q6/MainActivity.kt
@@ -1,23 +1,76 @@
 package com.example.assignmen2_q6
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.Spanned
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.assignmen2_q6.databinding.ActivityMainBinding
-import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var main_list = mutableListOf<Any>(0)
+
+    private fun reset_list(){
+        main_list = mutableListOf<Any>(0)
+    }
+
+
+
     private lateinit var binding: ActivityMainBinding
+
+    //private var numberInput: EditText = binding.workspace //this line crashes program
+
+
     private var canAddOperation = false
     private var canAddDecimal = true
+
+    private var holdingSqrt = false
+
+    //sourced from Java code found at https://stackoverflow.com/questions/3349121/how-do-i-use-inputfilter-to-limit-characters-in-an-edittext-in-android
+    private var decimalFilter:InputFilter = object : InputFilter {
+        override fun filter(
+            input: CharSequence, start: Int, end: Int,
+            dest: Spanned, dstart: Int, dend: Int
+        ): CharSequence {
+            return if (!canAddDecimal and input.contains('.')){
+                ""
+            } else {
+                input
+            }
+        }
+    }
+
+    //using text watcher to be 'smart' and avoid multiple decimals when inputting a number
+    //from keyboard
+    //source: https://www.geeksforgeeks.org/how-to-implement-textwatcher-in-android/
+
+    private val decimalLookout: TextWatcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            if (s.last() == '.') {
+                canAddDecimal = false
+            }
+        }
+        override fun afterTextChanged(s: Editable) {
+            if (!s.contains('.')){ //this covers when user backspaces and deletes a decimal
+                canAddDecimal = true
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.workspace.addTextChangedListener(decimalLookout) //not working as intended
+        binding.workspace.filters = arrayOf(decimalFilter)
+
         binding.button1.setOnClickListener { numberAction(it) }
         binding.button2.setOnClickListener { numberAction(it) }
         binding.button3.setOnClickListener { numberAction(it) }
@@ -34,7 +87,7 @@ class MainActivity : AppCompatActivity() {
         binding.button14.setOnClickListener { operationAction(it) }
         binding.button15.setOnClickListener { numberAction(it) }
         binding.button16.setOnClickListener { numberAction(it) }
-        binding.button17.setOnClickListener { equalsAction(it) }
+        //binding.button17.setOnClickListener { equalsAction(it) }
 
     }
 
@@ -59,6 +112,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /*
     fun equalsAction(view: View) {
         binding.workspace.text = calculateResults()
     }
@@ -170,5 +224,5 @@ class MainActivity : AppCompatActivity() {
 
         return list
     }
-
+    */
 }
